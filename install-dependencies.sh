98e34f5d0df5c451de5f1b38ecccdee9751048ef
Ryan Senanayake <rsenapps@gmail.com>
2014-12-29 02:38:13 -0800

    update build tools version

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -21,14 +21,11 @@ if [ ! -e $DEPS ]; then
   echo y | android update sdk -u -a -t platform-tools &&
   echo y | android update sdk -u -a -t tools &&
   echo y | android update sdk -u -a -t android-21 &&
-  echo y | android update sdk -u -a -t build-tools-21.1.2 &&
+  echo y | android update sdk -u -a -t build-tools-21.1.1 &&
   echo y | android update sdk -u -a -t extra-google-google_play_services &&
   echo y | android update sdk -u -a -t extra-android-m2repository &&
   echo y | android update sdk -u -a -t extra-android-support &&
   echo y | android update sdk -u -a -t extra-google-m2repository &&
-  echo y | android update sdk -u -a -t extra-google-play_licensing &&
-  echo y | android update sdk -u -a -t sys-img-x86-android-21 &&
-  echo y | android update sdk -u -a -t addon-google_apis-google-21 &&
 
   # DO NOT CREATE EMULATOR FOR THE MOMENT
   # echo n | android create avd -n testing -f -t android-21 &&
