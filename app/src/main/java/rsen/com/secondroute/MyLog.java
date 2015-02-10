d26af5b95fe2538f8fd4c96052972b00b40976fc
Ryan Senanayake <rsenapps@gmail.com>
2015-02-10 08:23:00 -0800

    provide more log info

--- app/src/main/java/rsen/com/secondroute/MyLog.java
+++ app/src/main/java/rsen/com/secondroute/MyLog.java
@@ -2,9 +2,13 @@ package rsen.com.secondroute;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
 import android.preference.PreferenceManager;
+import android.text.format.DateFormat;
 
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 
 public class MyLog {
@@ -30,6 +34,25 @@ public class MyLog {
     public static String getLog(Context c) {
         SharedPreferences prefs = PreferenceManager
                 .getDefaultSharedPreferences(c);
-        return prefs.getString("log", "Installed");
+        String log = "";
+        String[] allPrefs = prefs.getAll().keySet().toArray(new String[prefs.getAll().keySet().size()]);
+        Arrays.sort(allPrefs);
+        for (String key : allPrefs) {
+            log += "\n" + key + " = " + String.valueOf(prefs.getAll().get(key));
+        }
+
+        PackageInfo pInfo;
+        try {
+            pInfo = c.getPackageManager().getPackageInfo(c.getPackageName(), 0);
+            log += "\nApp Version: " + pInfo.versionName;
+        } catch (PackageManager.NameNotFoundException e) {
+        }
+        log += "\nManufacturer: " + android.os.Build.MANUFACTURER;
+        log += "\nDevice Name: " + android.os.Build.MODEL;
+        log += "\nAndroid version: " + android.os.Build.VERSION.SDK_INT;
+        log += "\nCurrent Time: "
+                + (DateFormat.format("dd-MM-yyyy hh:mm:ss",
+                new java.util.Date()).toString());
+        return log;
     }
 }
