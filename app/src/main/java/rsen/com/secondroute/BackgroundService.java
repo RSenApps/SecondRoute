bb39dd12f28e69dc6bdf29b46e4448271014d400
Ryan Senanayake <rsenapps@gmail.com>
2015-02-14 13:49:17 -0800

    handle no internet, add tester

--- app/src/main/java/rsen/com/secondroute/BackgroundService.java
+++ app/src/main/java/rsen/com/secondroute/BackgroundService.java
@@ -54,57 +54,56 @@ public class BackgroundService extends IntentService
         }
         ArrayList<Route> pr = BingMapsAPI.getDirectionsList(intent.getFloatExtra("lat", 0), intent.getFloatExtra("lng",0), lat, lng);
         List<String> cr = (List<String>) BingMapsAPI.getPreferredDirectionsList(this, ContextService.isHeadingHome);
-        double maxConfidenceScore = 0;
-        Route routeWithMaxConfidence = null;
-        for (Route route : pr)
+        if (pr != null && cr != null) //no network or error in network
         {
-            double confidenceScore = compareRoutes(route.instructions, cr);
-            MyLog.l("Route confidence: " + confidenceScore, this);
-            if (confidenceScore > maxConfidenceScore)
-            {
-                maxConfidenceScore = confidenceScore;
-                routeWithMaxConfidence = route;
-            }
-        }
-        MyLog.l("Max confidence (>.8): " + maxConfidenceScore, this);
-
-        if (maxConfidenceScore >= .8) { //need to find match with original route, but also the fastest route can't be the original
-            final double similarityScore = compareRoutes(pr.get(0).instructions, cr);
-            MyLog.l("Similarity score (<.9): " + similarityScore, this);
-
-            if (similarityScore <= .9) {
-                int timeDifference = routeWithMaxConfidence.durationMinutes - pr.get(0).durationMinutes;
-                int minDifference = PreferenceManager.getDefaultSharedPreferences(this).getInt("minDifference", 5);
-                MyLog.l("Difference in Time: " + timeDifference, this);
-
-                if (timeDifference >= minDifference) {
-                    Intent i = new Intent(this, FasterRouteActivity.class);
-                    i.putExtra("instruction", pr.get(0).instructions.get(0));
-                    i.putExtra("differenceInTime", routeWithMaxConfidence.durationMinutes - pr.get(0).durationMinutes);
-                    i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                    startActivity(i);
-                    Intent service = new Intent(this, ContextService.class);
-                    stopService(service);
+            double maxConfidenceScore = 0;
+            Route routeWithMaxConfidence = null;
+            for (Route route : pr) {
+                double confidenceScore = compareRoutes(route.instructions, cr);
+                MyLog.l("Route confidence: " + confidenceScore, this);
+                if (confidenceScore > maxConfidenceScore) {
+                    maxConfidenceScore = confidenceScore;
+                    routeWithMaxConfidence = route;
                 }
             }
-            else {
+            MyLog.l("Max confidence (>.8): " + maxConfidenceScore, this);
+
+            if (maxConfidenceScore >= .8) { //need to find match with original route, but also the fastest route can't be the original
+                final double similarityScore = compareRoutes(pr.get(0).instructions, cr);
+                MyLog.l("Similarity score (<.9): " + similarityScore, this);
+
+                if (similarityScore <= .9) {
+                    int timeDifference = routeWithMaxConfidence.durationMinutes - pr.get(0).durationMinutes;
+                    int minDifference = PreferenceManager.getDefaultSharedPreferences(this).getInt("minDifference", 5);
+                    MyLog.l("Difference in Time: " + timeDifference, this);
+
+                    if (timeDifference >= minDifference) {
+                        Intent i = new Intent(this, FasterRouteActivity.class);
+                        i.putExtra("instruction", pr.get(0).instructions.get(0));
+                        i.putExtra("differenceInTime", routeWithMaxConfidence.durationMinutes - pr.get(0).durationMinutes);
+                        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(i);
+                        Intent service = new Intent(this, ContextService.class);
+                        stopService(service);
+                    }
+                } else {
+                    mHandler.post(new Runnable() {
+                        @Override
+                        public void run() {
+
+                            Toast.makeText(BackgroundService.this, "The fastest route was too similar to the preferred route. Max was .9 and similarity score was: " + similarityScore, Toast.LENGTH_LONG).show();
+                        }
+                    });
+                }
+            } else {
+                final double mConfidence = maxConfidenceScore;
                 mHandler.post(new Runnable() {
                     @Override
                     public void run() {
-
-                        Toast.makeText(BackgroundService.this, "The fastest route was too similar to the preferred route. Max was .9 and similarity score was: " + similarityScore, Toast.LENGTH_LONG).show();
+                        Toast.makeText(BackgroundService.this, "No match was found with preferred route. Threshold was .9 and maximum confidence was: " + mConfidence, Toast.LENGTH_LONG).show();
                     }
                 });
             }
-        }
-        else {
-            final double mConfidence = maxConfidenceScore;
-            mHandler.post(new Runnable() {
-                @Override
-                public void run() {
-                    Toast.makeText(BackgroundService.this, "No match was found with preferred route. Threshold was .9 and maximum confidence was: " + mConfidence, Toast.LENGTH_LONG).show();
-                }
-            });
         }
     }
 
