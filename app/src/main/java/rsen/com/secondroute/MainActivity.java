f1dc53249142a5f164bcc88f6cc190bd8309566a
Ryan Senanayake <rsenapps@gmail.com>
2015-04-11 22:30:40 -0700

    email log

--- app/src/main/java/rsen/com/secondroute/MainActivity.java
+++ app/src/main/java/rsen/com/secondroute/MainActivity.java
@@ -77,6 +77,7 @@ public class MainActivity extends ActionBarActivity {
                 Intent emailIntent = new Intent(Intent.ACTION_SENDTO, Uri.fromParts(
                         "mailto", "rsenapps+secondroute@gmail.com", null));
                 emailIntent.putExtra(Intent.EXTRA_SUBJECT, "SecondRoute Log");
+                emailIntent.putExtra(Intent.EXTRA_TEXT, MyLog.getLog(MainActivity.this));
                 startActivity(Intent.createChooser(emailIntent, "Send email..."));
             }
         });
